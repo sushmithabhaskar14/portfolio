7fb2a930992f388ebf74ea28cfffc57634799286
sushmithabhaskar14 <sushmithabhaskar1495@gmail.com>
2024-07-01 00:13:44 +0530

    first commit

---
+++ app/assets/javascript/application.js
@@ -0,0 +1,10 @@
+//= require bootstrap.bundler.js
+//= require bootstrap.bundle.min.js
+//= require bootstrap.js
+//= require jquery.min.js
+//= require jquery3
+//= require popper
+//= require bootstrap
+//= require jquery
+//= require jquery_ujs
+
---
+++ db/migrate/20240630135456_create_blogs.rb
@@ -0,0 +1,11 @@
+class CreateBlogs < ActiveRecord::Migration[7.1]
+  def change
+    create_table :blogs do |t|
+      t.string :name
+      t.text :content
+      t.boolean :status
+
+      t.timestamps
+    end
+  end
+end
